b26493b42aa5d451cc667ae9213fb72f05183fe6
JasonPink79 <jaysunrose@aol.com>
2021-03-30 01:46:23 +0000

    traffic light with react

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+export function TrafficLight() {
+	const [color, setColor] = useState("red");
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex">
+			<div className="d-flex flex-column align-items-center justify-content-center">
+				<div className="trafficstick"></div>
+				<div className="lightbox d-flex flex-column align-items-center justify-content-center">
+					<div
+						className={
+							color === "red" ? "light red selected" : "light red"
+						}
+						onClick={() => setColor("red")}></div>
+
+					<div
+						className={
+							color === "yellow"
+								? "light yellow selected"
+								: "light yellow"
+						}
+						onClick={() => setColor("yellow")}></div>
+
+					<div
+						className={
+							color === "green"
+								? "light green selected"
+								: "light green"
+						}
+						onClick={() => setColor("green")}></div>
+				</div>
+			</div>
+			<div className="d-flex flex-column align-items-center justify-content-center">
+				<div className="signalbox d-flex align-items-center justify-content-center">
+					<i
+						className={
+							color === "red"
+								? "far fa-hand-paper"
+								: color === "yellow"
+								? "fas fa-exclamation-triangle"
+								: "fas fa-walking"
+						}></i>
+				</div>
+				<div className="signalpole"></div>
+			</div>
 		</div>
 	);
 }
